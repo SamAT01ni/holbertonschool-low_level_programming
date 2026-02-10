2472fce92218a8aba0df21d5816302b5eeebc575
SamAT01 <sam.a.thompson2001@gmail.com>
2026-02-10 06:23:18 +0000

    so many alphabets

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - lower AND uppercase!!!
+ *
+ * Return: 0
+ */
+
+
+int main(void)
+{
+	char ch = 'a';
+	char CH = 'A';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	while (CH <= 'Z')
+	{
+		putchar(CH);
+		CH++;
+	}
+	putchar('\n');
+	return (0);
+}
