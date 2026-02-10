b60c31cc18cec55d29ba0ac63d2011e77ea508bd
SamAT01ni <sam.a.thompson2001@gmail.com>
2026-02-10 01:07:39 +0000

    bit of a pain

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/*
+ * main - seeing the relation between the pevious number and a new one
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+	int x;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	x = n % 10;
+	if (x > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, x);
+	else if (x == 0)
+		printf("Last digit of %d is %d and is 0\n", n, x);
+	else
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, x);
+	return (0);
+}
