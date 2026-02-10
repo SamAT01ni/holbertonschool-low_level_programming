50695c46eccd79c723ddbb1faa8c7de3c665b01c
SamAT01 <sam.a.thompson2001@gmail.com>
2026-02-10 07:31:36 +0000

    6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - more complicated
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	while (n <= 9)
+	{
+		putchar('0' + n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
