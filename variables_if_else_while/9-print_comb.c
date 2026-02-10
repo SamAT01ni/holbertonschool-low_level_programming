434a8e5856eae91fb6f55215409b6db3975f29b2
SamAT01 <sam.a.thompson2001@gmail.com>
2026-02-10 08:13:41 +0000

    C project 2 bosh

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - seems deceiving
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar('0' + n);
+
+		if (n == 9)
+			continue;
+
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+	return (0);
+}
