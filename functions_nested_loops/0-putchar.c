4ced07311c19cd9521254975fb15289af60812c7
SamAT01ni <sam.a.thompson2001@gmail.com>
2026-02-11 05:14:26 +0000

    putchar?

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,2 +1,23 @@
 #include <stdio.h>
+#include <unistd.h>
 
+/**
+ * main - prints out _putchar
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	putchar ('_');
+	putchar ('p');
+	putchar ('u');
+	putchar ('t');
+	putchar ('c');
+	putchar ('h');
+	putchar ('a');
+	putchar ('r');
+
+	putchar ('\n');
+	return (0);
+}
