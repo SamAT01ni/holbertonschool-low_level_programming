6ab9f8e49eb3f3a90a3e50f52f0d9cc16010d9f7
SamAT01ni <sam.a.thompson2001@gmail.com>
2026-02-11 11:38:38 +0000

    9 and update main.h

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ * times_table - prints the times tables
+ *
+ * Return: void
+ */
+
+void times_table(void)
+{
+	int n1 = 0;
+	int n2;
+	int sum;
+	int first;
+	int sec;
+
+	while (n1 <= 9)
+	{
+		n2 = 0;
+
+		while (n2 <= 9)
+		{
+			sum = n1 * n2;
+			first = sum / 10;
+			sec = sum % 10;
+
+			if (n2 == 0)
+			{
+				_putchar('0' + sum);
+			}
+			else if (sum <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + sum);
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar('0' + first);
+				_putchar('0' + sec);
+			}
+
+			n2++;
+
+		}
+
+		_putchar('\n');
+		n1++;
+	}
+}
