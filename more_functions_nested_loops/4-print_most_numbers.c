2d1b6455881c40a321b11d40d2257e2cb7373471
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-02-16 03:09:48 +0000

    4 and main

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - dont print 2 or 4 bc they are in the files
+ *
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int i = 0;
+	while (i <= 9)
+	{
+		if (i !=2 && i != 4)
+		{
+			_putchar(i + '0');
+			i = i + 1;
+		}
+		else
+		{
+			i = i + 1;
+			continue;
+		}
+	}
+	_putchar('\n');
+}
