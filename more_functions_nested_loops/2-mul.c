c4dd432d48fecfa00ddb176112daf0a58866deee
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-02-16 02:07:28 +0000

    a

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiply
+ * @a: integer 1
+ * @b: integer 2
+ *
+ * Return: product of a and b
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
