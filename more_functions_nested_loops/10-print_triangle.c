9945fb219fab794931d82c1f0cdc2703514c6881
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-02-17 00:14:50 +0000

    triangles yo!

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * print_triangle - right angle triangle push right
+ * @size: size of the triangle in hashs and lines
+ *
+ * Return: big ass triangle
+ */
+
+void print_triangle(int size)
+{
+	int gaps;
+	int tris;
+	int i = 1;
+
+	if (size == 0)
+	{
+		_putchar('\n');
+	}
+	while (i <= size)
+	{
+		tris = 0;
+		gaps = size - i;
+
+		while (gaps > 0)
+		{
+			_putchar(' ');
+			gaps = gaps - 1;
+		}
+		while (tris < i)
+		{
+			_putchar('#');
+			tris = tris + 1;
+		}
+		i = i + 1;
+		_putchar('\n');
+	}
+}
