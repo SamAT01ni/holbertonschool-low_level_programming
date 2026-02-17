46f78a7e54f9f3bc403e01306f9949b297241a25
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-02-17 04:22:33 +0000

    task 7

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -11,13 +11,14 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\0')
+	while (*str)
 	{
 		if (i % 2 == 0)
 		{
-			_putchar(str[i]);
+			_putchar(*str);
 		}
 		i = i + 1;
+		str = str + 1;
 	}
 	_putchar('\n');
 }
