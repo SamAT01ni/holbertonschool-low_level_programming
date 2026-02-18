8c8348371c86847ef94c50533d4d326f8f797aa1
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-02-18 03:32:31 +0000

    task 0

---
+++ 2-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcat - makes a function to concatenates 2 strings
+ * @dest - first string
+ * @src - 2nd string
+ *
+ * Return: 0
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i = i + 1;
+	}
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i = i + 1;
+		j = j + 1;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
