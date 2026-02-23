4c8312a9fafade05bfe692fb88038e055d5dc9c4
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-02-23 06:21:24 +0000

    task 4

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strpbrk - locates first appearance of a byte in accept
+ * @s: string checked
+ * @accept: accepted characters
+ *
+ * Return: pointer to byte s that matches, else NULL
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i = 0;
+	int j;
+
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (accept[j] == s[i])
+			{
+				return (&s[i]);
+			}
+		}
+	}
+	return (NULL);
+}
