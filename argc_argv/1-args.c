4b0b393e16480a15389ef8e9ec344ab17cf2b2f7
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-02-27 23:14:37 +0000

    task 1 arg count

---
+++ argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints number of args
+ * @argc: count
+ * @argv: data
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+	(void)argv;
+}
