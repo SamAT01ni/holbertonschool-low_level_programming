eb569fb4207a0d9d20d641bbdaf3b3f466d816f2
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-03-25 07:48:47 +0000

    task 7

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -13,7 +13,23 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new;
 	dlistint_t *current;
-	unsigned int i;
+	unsigned int i = 0;
+
+	current = *h;
+
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	while (current && i < idx - 1)
+	{
+		current = current->next;
+		i++;
+	}
+
+	if (current == NULL)
+		return (NULL);
+	if (current->next == NULL)
+		return (add_dnodeint_end(h, n));
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
@@ -21,16 +37,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	new->n = n;
 
-	current = *h;
-	for (i = 0; current && current->next && i < idx; i++)
-	{
-		current = current->next;
-	}
+	new->next = current->next;
+	new->prev = current;
 
-	if (current)
-	{
-		new->prev = current;
-		current->next = new;
-	}
+	current->next->prev = new;
+	current->next = new;
+
 	return (new);
 }
