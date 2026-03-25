8313605c5f828e3d069ce7bfba1999618869f550
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-03-25 00:17:27 +0000

    readding files

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - adds a node at the beginning of double list
+ * @head: start of list
+ * @n: integer
+ *
+ * Return: address of new element or NULL
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->prev = NULL;
+	new->next = *head;
+
+	if (*head)
+		(*head)->prev = new;
+
+	*head = new;
+	return (new);
+}
