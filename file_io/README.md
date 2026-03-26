8eea1bcff338dd0984f9492316d0884dd5d0d2e1
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-03-26 23:48:58 +0000

    header initialised and readme added

---
+++ file_io/README.md
@@ -0,0 +1,3 @@
+What up!
+
+file.io sounds like a website but that doesnt really matter does it
---
+++ file_io/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <string.h>
+
+#include <fcntl.h>   /* open */
+#include <unistd.h>  /* read, write, close */
+#include <stdlib.h>  /* exit */
+#include <string.h>  /* strlen */
+#include <sys/stat.h>   // permission macros
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
