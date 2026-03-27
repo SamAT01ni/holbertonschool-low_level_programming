038089d5b9c3dd5fd377f5cf57acc70fb48f1f84
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-03-27 00:13:52 +0000

    update

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -10,13 +10,13 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, i;
+	int fd, i, help;
 
 	if (filename == NULL)
 		return (-1);
 
 	fd = open(filename,
-			O_WRONLY | O_CREAT | O_TRUNC,
+			O_CREAT | O_WRONLY | O_TRUNC,
 			S_IRUSR | S_IWUSR);
 
 	if (fd == -1)
@@ -26,8 +26,8 @@ int create_file(const char *filename, char *text_content)
 	{
 		for (i = 0; text_content[i] != '\0'; i++)
 			;
-		write(fd, text_content, i);
-		if (write(fd, text_content, i) == -1)
+		help = write(fd, text_content, i);
+		if (help == -1)
 			return (-1);
 	}
 	close(fd);
